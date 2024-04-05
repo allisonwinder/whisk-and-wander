dd13d32c4ff70e6bd904dc918804d27b8136cfd0
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2024-04-05 15:25:45 -0600

    navigation buttons and me pic sized correctly

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,18 +4,16 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <nav>
-      <div className="btn">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/addRecipe">Add Recipe</Link>
-          </li>
-          <li>
-            <Link to="/recipes">All Recipes</Link>
-          </li>
-        </ul>
+      <div className="buttonWrapper">
+        <Link to="/" className="btn">
+          Home
+        </Link>
+        <Link to="/addRecipe" className="btn">
+          Add Recipe
+        </Link>
+        <Link to="/recipes" className="btn">
+          All Recipes
+        </Link>
       </div>
     </nav>
   );
