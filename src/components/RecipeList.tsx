ca775badf56370bea9712ae393516282feb1ff63
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2024-04-08 10:45:46 -0600

    we got a basic database now!

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,3 +1,24 @@
+import React, { useState, useEffect } from "react";
+
 export default function RecipeList() {
-  return <h1 className="brand">All the Recipes</h1>;
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="brand">All the Recipes</h1>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
